baf00d3040da23194f5cdc1a034ed7ed2e4a7e7b
pooja11021999 <poojagaikwad.tg@gmail.com>
2024-09-11 12:23:42 +0530

    Initial commit

---
+++ app/(tabs)/third.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import CustomText from "../../assets/commonElements/text";
+
+const Third = () => {
+  return (
+    <View>
+      <CustomText text={'firstRoute - return to the first screen defined in the navigator (default) initialRoute - return to initial screen passed in initialRouteName prop, if not passed, defaults to the first screen order- return to screen defined before the focused screen history - return to last visited screen in the navigator; if the same screen is visited multiple times, the older entries are dropped from the history none - do not handle back button'}
+      externalStyle={{color:'black'}}/>
+    </View>
+  );
+};
+
+export default Third;
---
+++ app/_layout.js
@@ -0,0 +1,37 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Index from "./(tabs)";
+import First from "./(tabs)/first";
+import Second from "./(tabs)/second";
+import Third from "./(tabs)/third";
+
+export default function Layout() {
+  const Tab = createMaterialTopTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Basic Information"
+        screenOptions={{
+          tabBarScrollEnabled: true,
+          tabBarActiveTintColor: "#00477f",
+          tabBarInactiveTintColor: "#B2BEB5",
+          tabBarIndicatorStyle: {
+            backgroundColor: "#00477f",
+          },
+          tabBarLabelStyle: {
+            flex: 1,
+            maxWidth: 250,
+          },
+          tabBarItemStyle: {
+            width: "auto",
+          },
+        }}
+      >
+        <Tab.Screen name="Basic Information" component={Index} />
+        <Tab.Screen name="Owner Card" component={First} />
+        <Tab.Screen name="Social Information" component={Second} />
+        <Tab.Screen name="Other" component={Third} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
